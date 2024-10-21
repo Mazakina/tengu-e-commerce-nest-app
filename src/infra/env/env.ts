1f8b0e896bbb935bafceb18deb7448f5e7d1b688
Paulo Mazakina <8.mazakina@gmail.com>
2024-10-21 18:18:59 -0300

    chore: update script for production deployment

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -13,6 +13,7 @@ export const envSchema = z.object({
   REDIS_HOST: z.string().optional().default('127.0.0.1'),
   REDIS_PORT: z.coerce.number().optional().default(6379),
   REDIS_DB: z.coerce.number().optional().default(0),
+  REDIS_PASSWORD: z.coerce.string().optional().default(''),
 });
 
 export type EnvSchemaType = z.infer<typeof envSchema>;
