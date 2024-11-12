40907420ff6fc4fe811f459d1df2095a6c3ebde4
Paulo Mazakina <8.mazakina@gmail.com>
2024-11-12 02:09:10 -0300

    task: updating for production

--- src/infra/cache/redis/redis-cache-repository.ts
+++ src/infra/cache/redis/redis-cache-repository.ts
@@ -6,7 +6,7 @@ import { RedisService } from './redis.service';
 export class RedisCacheRepository implements CacheRepository {
   constructor(private redis: RedisService) {}
   async set(key: string, value: string): Promise<void> {
-    await this.redis.set(key, value, 'EX', 60 * 2);
+    await this.redis.set(key, value, 'EX', 60 * 15);
   }
   async get(key: string): Promise<string | null> {
     return this.redis.get(key);
